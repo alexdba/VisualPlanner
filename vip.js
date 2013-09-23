3c4e19f4b396bb80c60fc0dee1eee41abe0eb95a
ct6787@gmail.com <ct6787@gmail.com@9cb847ec-30cc-6a4e-15ac-0ed2bf107903>
2013-09-23 19:43:15 +0000

    add tracking code

--- vip.js
+++ vip.js
@@ -15,6 +15,16 @@ var next_event_req;
 var selection_cell_start;
 var selection_cell_end;
 
+// tracking code
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-44262361-1']);
+_gaq.push(['_trackPageview']);
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
 // disable default selection behaviour
 document.body.style.webkitUserSelect = "none"; // ch
 document.body.style.MozUserSelect = "none"; // ff
